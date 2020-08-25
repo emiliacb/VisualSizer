56a88a2f02fbb7c1a90176af67f625cb675e08e4
Eme <bramasca@gmail.com>
2020-08-24 23:58:16 -0300

    fifth version

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # VisualSizer
 
 This app allow you to obtain the code to embed a youtube video, resizing it in a visual form.
+
 https://em3c2.github.io/VisualSizer/
 
-### Pendding:
--  [x] Add "Copy" button 
--  [x] Change CSS resize property with javascript vanilla functions, in order to mobile compatibility
+
+
+
 
 ### First version:
 
@@ -25,3 +26,10 @@ Added "Copy" buttom and more style changes.
 Changed the "resize" from CSS to JS vainilla, I learned to much doing this.
 
 <img width="300px" src="./img/iter4.png">
+
+### fifth version:
+Fixeds bugs.
+
+<img width="300px" src="./img/iter5.png">
+
+<img width="300px" src="./img/iter5m.png">
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 		//Obteniendo elementos
 
-		var clases = ['column', 'urlVideo', 'botton', 'container', 'embedVideo', 'flecha', 'size', 'code','copyBtn'];
+		var clases = ['body','column', 'urlVideo', 'botton', 'container', 'embedVideo', 'flecha', 'size', 'code','copyBtn'];
 		var el = {};
 
 		clases.forEach((e,i) => {
@@ -55,22 +55,35 @@ var mouseTouchYPrev = 0
 var containerXPrev  = 160
 var containerYPrev  = 90
 
+
 	const  resizeX = e => {
 				el.container.style.width =(containerXPrev * -1.3 + (mouseXPrev - e.screenX)) * -1 + 'px'
-				//el.container.style.height = el.container.style.width.replace('px','') / 1.7 + 'px'
 				el.container.style.height = (containerYPrev * -1.3 + (mouseYPrev - e.screenY)) * -1 + 'px'
+				document.documentElement.style.setProperty('--widthContainer', el.container.style.width)
 			}
 
 	const  resizeXTouch = e => {
 				el.container.style.width = (containerXPrev * -1.1+ (mouseTouchXPrev - e.changedTouches[0].screenX)) *  -1 +  'px'
-				//el.container.style.height = el.container.style.width.replace('px','') / 1.7 + 'px'
 				el.container.style.height = (containerYPrev * -1.1 + (mouseTouchYPrev - e.changedTouches[0].screenY)) * -1 + 'px'
+				document.documentElement.style.setProperty('--widthContainer', el.container.style.width)
 			}
 
 
+	el.flecha.addEventListener('dblclick', e => {
+		e.preventDefault()
+		document.documentElement.style.setProperty('--widthContainer', '0px')
+		setTimeout(() => {
+		document.documentElement.style.setProperty('--widthContainer', el.container.style.width)
+		},300)
+	})
+	
+
 
 	const removeResize = e => {
-			e.preventDefault()
+			//e.preventDefault()
+
+			el.body.style.overflow = 'auto'
+			el.body.style.touchAction = 'auto'
 
 			el.size.innerHTML = `${el.container.offsetWidth}x${el.container.offsetHeight}`;
 			el.code.value =  `<iframe width="${el.container.offsetWidth}" height="${el.container.offsetHeight} "id="video" src="https://www.youtube.com/embed/${idVideo}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
@@ -87,13 +100,22 @@ var containerYPrev  = 90
 
 						containerXPrev  = el.container.style.width.replace('px','')  - 30
 						containerYPrev  = el.container.style.height.replace('px','')  - 30 
-			},20)			
+
+						mouseXPrev = 0
+						mouseYPrev = 0
+						mouseTouchXPrev = 0
+					 	mouseTouchYPrev = 0
+
+			},50)			
 	}
 
 	// Evento
 
 		el.flecha.addEventListener('mousedown', (e) => {
-			console.log('se hizo click')
+			
+			el.body.style.overflow = 'hidden'
+			el.body.style.touchAction = 'none'
+
 			mouseXPrev = e.screenX
 			mouseYPrev = e.screenY
 
@@ -103,7 +125,7 @@ var containerYPrev  = 90
 			el.embedVideo.addEventListener('mousemove', resizeX)
 
 			window.addEventListener('mouseup', removeResize)
-			console.log('ya están los eventos')
+
 	})
 
 		el.flecha.addEventListener('touchstart', (e) => {
